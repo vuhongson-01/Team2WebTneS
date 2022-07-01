dc81df2245ad5a83c291ac38621f93b06134fe18
Chinh Pham Ba <chinhthoinl@gmail.com>
2022-07-01 17:51:41 +0700

    Update login page full html/css

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -5,35 +5,43 @@ import './LoginForm.css';
 function LoginForm() {
 
     return (
-        <div className="login-form">
-            <form>
-                <div className="input-container">
-                    <label>Enter username
+        <div className="container">
+            <div className="login-form">
+                <form>
+                    <div className="input-container">
+                        <label>Enter username
+                            <input
+                            type="text" 
+                            placeholder="username"
+                            />
+                        </label>
+                    </div>
+                    <div className="input-container">
+                        <label>Enter password
+                            <input
+                            type="password"
+                            placeholder="password"
+                            />
+                        </label>
+                    </div>
+                    <div className="check-box">
+                        <label htmlFor="check-box-login">Keep signed in?</label>
                         <input
-                        type="text" 
-                        placeholder="username"
+                        type="checkbox"
+                        id="check-box-login"
                         />
-                    </label>
+                    </div>
+                    <div className="button-container">
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </div>
+            <div className="logo">
+                <div className="__logo">
+                    <a className="logo" href="./"><i class="fa-solid fa-utensils"></i><span className="nhoam-logo">Nhoàm</span></a>
+                    <span>restaurant</span>
                 </div>
-                <div className="input-container">
-                    <label>Enter password
-                        <input
-                        type="password"
-                        placeholder="password"
-                        />
-                    </label>
-                </div>
-                <div className="check-box">
-                    <label htmlFor="check-box-login">Keep signed in?</label>
-                    <input
-                    type="checkbox"
-                    id="check-box-login"
-                    />
-                </div>
-                <div className="button-container">
-                    <button type="submit">Login</button>
-                </div>
-            </form>
+            </div>
         </div>
     )
 

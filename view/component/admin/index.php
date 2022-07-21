322b00bfbcfb75c5b83c9bd59bdcfd434dabcf8e
Vũ Sơn <vuhongsonchv1619gmail.com@MacBook-Pro-cua-Vu-2.local>
2022-07-21 15:51:33 +0700

    admin

---
+++ view/component/admin/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="./view/style/admin.css" />
+    </head>
+    <body>
+        <div class="admin">
+            <div class="admin-header">
+                <a class="logout-btn">LOGOUT</a>
+            </div>
+            <div class="admin-state">
+                <div class="table-map">
+                    <img src="./view/asset/map.jpg" width="100%"/>
+                </div>
+                <div class="table-process">
+                </div>
+            </div>
+        </div>    
+    </body>    
+</html>
+
+
